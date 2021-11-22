b6c9fc64fae0ff7caa7ba713f51ffc81227ec09c
Serge <s.a.kostrikin@gmail.com>
2021-11-22 10:52:45 -0500

    Add possibility to close color-picker by pressing on ESC btn.

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -18,6 +18,18 @@ const Canvas = () => {
   const contextRef = useRef(null)
 
   useEffect(() => {
+    const closeColorPickerByEsc = (e) => {
+      if (e.key === 'Escape') {
+        setShowColorPicker(false)
+      }
+    }
+
+    document.addEventListener('keydown', closeColorPickerByEsc)
+
+    return () => document.removeEventListener('keydown', closeColorPickerByEsc)
+  }, [])
+
+  useEffect(() => {
     const canvas = canvasRef.current
     canvas.width = document.body.clientWidth
     canvas.height = document.body.clientHeight
